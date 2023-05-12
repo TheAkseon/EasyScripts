5173040b77174b7e6ab9c4c0a355cc16455416ec
TheAkseon <abqwerty390@gmail.com>
2023-05-12 15:38:30 +0300

    add project

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.AddForce(Vector3.forward * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _rigidbody.AddForce(Vector3.back * _speed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rigidbody.AddForce(Vector3.left * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.AddForce(Vector3.right * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector3.up * _speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private SpawnPoint[] _spawnPoints;
+    [SerializeField] private GameObject _prefab;
+
+    private void Start()
+    {
+        _spawnPoints = GetComponentsInChildren<SpawnPoint>();
+    }
+
+    private void Update()
+    {
+        Instantiate(_prefab, _spawnPoints[Random.Range(0, _spawnPoints.Length)].transform.position, Quaternion.identity);
+    }
+}
